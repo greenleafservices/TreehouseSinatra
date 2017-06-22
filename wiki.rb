4ff8488810e431c5ac6394820c824e09dca225ef
John Stegman <greenleafservices.gls@gmail.com>
2017-06-22 17:10:12 -0400

    End of wiki lessons

--- wiki.rb
+++ wiki.rb
@@ -7,7 +7,18 @@ rescue Errno::ENOENT
   return nil
 end
 
-get "/" do
+def save_content(title, content)
+  File.open("pages/#{title}.txt", "w") do |file|
+    file.print(content)
+  end
+end
+
+def delete_content(title)
+  File.delete("pages/#{title}.txt")
+end
+
+# ****************** Routes ******************
+get "/" do #root route
   erb :welcome
 end
 
@@ -15,33 +26,41 @@ get "/test" do
   erb :test
 end
   
-
-#get "/:title" do
-#  page_content(params[:title])
-#end
 get "/new" do
   erb :new
 end
 
 get "/:title" do
   @title = params[:title]
+  #Use the Page_content method (above) to get the file contents
   @content = page_content(@title)
   erb :show
 end
-# {"title"=>"Test", "content"=>"Test Content"}
+
+get "/:title/edit" do
+  @title = params[:title]
+  #Use the Page_content method (above) to get the file contents
+  @content = page_content(@title)
+  erb :edit
+end
+
 post "/create" do
   #params.inspect
   #resule of params.inspect is
-  # {"title"=>"Test", "content"=>"Test Content"}
-  #pass this to the save_content process
+  # {"title"=>"Entered title", "content"=>"Entered Content"}
+  #pass these to the save_content method
   save_content(params["title"], params["content"])
+  #go back to the page you just wrote
+  redirect URI.escape("/#{params["title"]}")
+end
+#The put request will update the page with any new data entered by the edit process
+put "/:title" do
+  save_content(params["title"], params["content"])
+  #go back to the page you just wrote
   redirect URI.escape("/#{params["title"]}")
 end
 
-def save_content(title, content)
-  File.open("pages/#{title}.txt", "w") do |file|
-    file.print(content)
-  end
+delete "/:title" do
+  delete_content(params[:title])
+  redirect "/"
 end
-
-#test comment
